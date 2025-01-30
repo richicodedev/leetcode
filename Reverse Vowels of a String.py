a6464155998939ce2c397df7a99e1fb332537f80
richicodedev <richi.codedev@gmail.com>
2025-01-30 17:53:44 +0100

    Initial commit

---
+++ Reverse Vowels of a String.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vow_in_s = []
+        slen = len(s)
+        vow = "aeiou"
+
+        for i in range(slen):
+            if s[i].lower() in vow:
+                vow_in_s.append(s[i])
+
+        vow_in_s.reverse()
+        dq = deque(vow_in_s)
+
+        res = ""
+        for i in range(slen):
+            if s[i].lower() in vow:
+                res += dq.popleft()
+            else:
+                res += s[i]
+        return res
